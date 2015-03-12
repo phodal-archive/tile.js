6bc904816d83b2a38c613771216e1113e5dcedeb
Fengda HUANG <h@phodal.com>
2015-03-12 17:45:49 +0800

    pass jshint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,12 @@
-$(function() {
-  var c = $("canvas")[0].getContext("2d");
+/* jshint bitwise:false */
+
+$(function () {
+  'use strict';
+  var c = $('canvas')[0].getContext('2d');
 
   var scene = {
     layers: [],
-    renderLayer: function(layer) {
+    renderLayer: function (layer) {
       // data: [array of tiles, 1-based, position of sprite from top-left]
       // height: integer, height in number of sprites
       // name: "string", internal name of layer
@@ -13,56 +16,61 @@ $(function() {
       // width: integer, width in number of sprites
       // x: integer, starting x position
       // y: integer, starting y position
-      if (layer.type !== "tilelayer" || !layer.opacity) { return; }
+      if (layer.type !== 'tilelayer' || !layer.opacity) {
+        return;
+      }
       var s = c.canvas.cloneNode(),
         size = scene.data.tilewidth;
-      s = s.getContext("2d");
+      s = s.getContext('2d');
       if (scene.layers.length < scene.data.layers.length) {
-        layer.data.forEach(function(tile_idx, i) {
-          if (!tile_idx) { return; }
-          var img_x, img_y, s_x, s_y,
+        layer.data.forEach(function (titleIndex, i) {
+          if (!titleIndex) {
+            return;
+          }
+          var imgX, imgY, sizeX, sizeY,
             tile = scene.data.tilesets[0];
-          tile_idx--;
-          img_x = (tile_idx % (tile.imagewidth / size)) * size;
-          img_y = ~~(tile_idx / (tile.imagewidth / size)) * size;
-          s_x = (i % layer.width) * size;
-          s_y = ~~(i / layer.width) * size;
-          s.drawImage(scene.tileset, img_x, img_y, size, size,
-            s_x, s_y, size, size);
+          titleIndex--;
+          imgX = (titleIndex % (tile.imagewidth / size)) * size;
+          imgY = ~~(titleIndex / (tile.imagewidth / size)) * size;
+          sizeX = (i % layer.width) * size;
+          sizeY = ~~(i / layer.width) * size;
+          s.drawImage(scene.tileset, imgX, imgY, size, size,
+            sizeX, sizeY, size, size);
         });
         scene.layers.push(s.canvas.toDataURL());
         c.drawImage(s.canvas, 0, 0);
       }
       else {
-        scene.layers.forEach(function(src) {
+        scene.layers.forEach(function (src) {
           var i = document.createElement('img');
           i.src = src;
           c.drawImage(i, 0, 0);
         });
       }
     },
-    renderLayers: function(layers) {
+    renderLayers: function (layers) {
       function isObject(obj) {
-        return typeof obj === 'array';
+	      var type = typeof obj;
+        return type === 'array';
       }
 
       layers = isObject(layers) ? layers : this.data.layers;
       layers.forEach(this.renderLayer);
     },
-    loadTileset: function(json) {
+    loadTileset: function (json) {
       this.data = json;
       this.tileset = document.createElement('img');
       this.tileset.src = json.tilesets[0].image;
       this.tileset.onload = $.proxy(this.renderLayers, this);
     },
-    load: function(name) {
+    load: function (name) {
       return $.ajax({
-        url: "./images/" + name + ".json",
-        dataType: "JSON",
+        url: './images/' + name + '.json',
+        dataType: 'JSON',
         type: 'get'
       }).done($.proxy(this.loadTileset, this));
     }
   };
 
-  scene.load("mountain");
+  scene.load('mountain');
 });
