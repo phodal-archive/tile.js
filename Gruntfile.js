7d5d5cea99ae18cdd2d3ea95dea7d39e3fe94208
Fengda HUANG <h@phodal.com>
2015-03-14 08:19:22 +0800

    gh-pages init

---
+++ Gruntfile.js
@@ -0,0 +1,115 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: '.'
+        }
+      }
+    },
+
+    jasmine : {
+      src : 'dist/tile.js',
+      options : {
+        host: "http://0.0.0.0:8000",
+        vendor: ['node_modules/jasmine-ajax/lib/mock-ajax.js'],
+        specs : 'specs/*-spec.js',
+        template: require('grunt-template-jasmine-istanbul'),
+        templateOptions: {
+          coverage: 'coverage/coverage.json',
+          report: {
+            type: 'lcov',
+            options: {
+              dir: 'coverage'
+            }
+          },
+          thresholds: {
+            lines: 90,
+            statements: 90,
+            branches: 75,
+            functions: 85
+          }
+        }
+      }
+    },
+    concat: {
+      options: {
+        separator: "\n\n"
+      },
+      dist: {
+        src: [
+          'src/_intro.js',
+          'src/main.js',
+          'src/_outro.js'
+        ],
+        dest: 'dist/<%= pkg.name.replace(".js", "") %>.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name.replace(".js", "") %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name.replace(".js", "") %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+
+    jshint: {
+      files: ['dist/lettuce.js'],
+      options: {
+        globals: {
+          console: true,
+          module: true,
+          document: true
+        },
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['concat', 'jshint', 'qunit']
+    },
+
+    replace: {
+      dist: {
+        options: {
+          patterns: [
+            {
+              match: 'version',
+              replacement: '<%= pkg.version %>'
+            }
+          ]
+        },
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ['dist/tile.js', 'dist/tile.min.js'],
+            dest: 'dist/'
+          }
+        ]
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-replace');
+
+  grunt.registerTask('test', ['replace', 'jshint', 'connect', 'jasmine']);
+  grunt.registerTask('default', ['concat','replace', 'jshint', 'connect', 'jasmine', 'uglify']);
+
+};
---
+++ README.md
@@ -0,0 +1,17 @@
+#Tile.js
+
+> A Pure JavaScript tiled map render. 仅~1.2kb
+
+``注意``: 仅支持正边形
+
+##Screenshot
+
+![Screenshot](./screenshot.jpg)
+
+basis on [https://gist.github.com/shaneriley/4078905](https://gist.github.com/shaneriley/4078905)
+
+## License
+
+© 2015 [Phodal Huang](http://www.phodal.com). This code is distributed under the MIT license. See `LICENSE.txt` in this directory.
+
+[待我代码编成，娶你为妻可好](http://www.xuntayizhan.com/person/ji-ke-ai-qing-zhi-er-shi-dai-wo-dai-ma-bian-cheng-qu-ni-wei-qi-ke-hao-wan/)
